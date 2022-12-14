0c34e18bd6779f3958bef36504dbc5176399a67e
Loki99911 <nikita_kresik@i.ua>
2022-12-14 14:54:15 +0200

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,61 +5,6 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   countTotalFeedback = () => {
-//     return this.state.good + this.state.neutral + this.state.bad;
-//   };
-//   countPositiveFeedbackPercentage = () => {
-//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-//   };
-//   incrementFeedback = event => {
-//     const nameBtn = event;
-//     this.setState({ [nameBtn]: this.state[nameBtn] + 1 });
-
-//   };
-
-//   render() {
-//     return (
-//       <div
-//         style={{
-//           // height: '100vh',
-//           display: 'flex',
-//           flexDirection: 'column',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//           fontSize: 40,
-//           color: '#010101',
-//         }}
-//       >
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.incrementFeedback}
-//           />
-//         </Section>
-//         <Section title="Statistics">
-//           {this.countTotalFeedback() === 0 ? (
-//             <Notification message="There is no feedback :( " />
-//           ) : (
-//             <Statistics
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
-
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
